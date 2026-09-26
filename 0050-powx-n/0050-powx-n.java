be9f06c0edc22d1f1aa1b39c892a94b738a8ae84
Aditya Bhosale <aadibhosale001@gmail.com>
2026-09-26 09:55:27 +0530

    Time: 1 ms (9.24%), Space: 48.4 MB (10.73%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,26 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return power(x, N);
+    }
+
+    public double power(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        double halfPow = power(x, n / 2);
+
+        if (n % 2 == 0) {
+            return halfPow * halfPow;
+        } else {
+            return x * halfPow * halfPow;
+        }
+    }
+}
